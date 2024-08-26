2c019a5aa83c5750721d21c965eb26aea38a156e
jgvlf <joaogabrielvlf@gmail.com>
2024-08-26 13:30:56 -0300

    Add typing and data type modules

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,7 @@ mod modules;
 
 fn main() {
     modules::guessing_game::main::guessing_game_run()
+    // modules::three::variables_mutability::variables::error_bind();
+    // modules::three::variables_mutability::variables::shadowing();
+    // modules::three::variables_mutability::variables::mut_error();
 }
--- src/modules/three/variables_mutability.rs
+++ src/modules/three/variables_mutability.rs
@@ -1,1 +1,25 @@
+pub mod variables {
+    pub fn error_bind() {
+        let mut x = 5;
+        println!("The value of x is: {x}");
+        x = 6;
+        println!("The value of x is: {x}");
+    }
+    pub fn shadowing() {
+        let x = 5;
 
+        let x = x + 1;
+
+        {
+            let x = x * 2;
+            println!("The value of x is: {x}")
+        }
+
+        println!("The value of x is: {x}")
+    }
+    pub fn mut_error() {
+        let spaces = "    ";
+        let spaces: usize = spaces.len();
+        println!("{spaces}");
+    }
+}
