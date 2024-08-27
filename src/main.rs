01f1d07e530f704396b3cae5ae822a27da7787b3
joaogabrielvlf <joaogabrielvlf@gmail.com>
2024-08-27 20:19:24 -0300

    Edit & Adding functions.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod modules;
 
 fn main() {
-    modules::guessing_game::main::guessing_game_run()
-    // modules::three::variables_mutability::variables::error_bind();
-    // modules::three::variables_mutability::variables::shadowing();
-    // modules::three::variables_mutability::variables::mut_error();
+    // modules::guessing_game::main::guessing_game_run();
+    println!("code execute in \"main()\" function");
+    modules::three::functions::another_function(5);
+    modules::three::functions::print_label_meansurement(900, 'W')
 }
