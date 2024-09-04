f89a43c36851343e416705c91c10ebebf7ce5624
jgvlf <joaogabrielvlf@gmail.com>
2024-09-04 13:59:32 -0300

    Printing with "dbg"

--- src/modules/structures/rectangles.rs
+++ src/modules/structures/rectangles.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -13,8 +14,19 @@ pub fn exe() {
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
     );
+    show_rect_structure()
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
+}
+
+fn show_rect_structure() {
+    let scale: u32 = 2;
+    let rect: Rectangle = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+    dbg!(&rect); // inline
+                 // println!("{rectangle:#?}"); //prety
 }
