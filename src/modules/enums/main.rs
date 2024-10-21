f2878ab965e700fa9790c170cbe9961903c9eecf
jgvlf <joaogabrielvlf@gmail.com>
2024-10-21 16:33:08 -0300

    Allow `dead_code` and `unused_variables`.

--- src/modules/enums/main.rs
+++ src/modules/enums/main.rs
@@ -1,11 +1,13 @@
+#[allow(dead_code)]
 enum IpAddr {
     V4(String),
     V6(String),
 }
 
+#[allow(dead_code)]
+#[allow(unused_variables)]
 pub fn main() {
     let home: IpAddr = IpAddr::V4(String::from("127.0.0.1"));
-
     let loopback: IpAddr = IpAddr::V6(String::from("::1"));
 
 }
