d817b4958b2b22273906c8e24ccc45764c7ec880
jgvlf <joaogabrielvlf@gmail.com>
2025-01-06 11:26:25 -0300

    Rust - Error Handling - Improve Code: Define Path
    This changes will try to get the file in the determined path using the "env::current_dir()" to get the working directory and join the remaining paths.

--- rust-error-handling/src/example/username.rs
+++ rust-error-handling/src/example/username.rs
@@ -1,8 +1,11 @@
 use std::fs::File;
 use std::io::{self, Read};
+use std::path::PathBuf;
 
 pub fn read_username_from_file() -> Result<String, io::Error> {
-    let username_file_result = File::open("hello.txt");
+    let current_dir: PathBuf = std::env::current_dir().unwrap();
+    let user_data_file: PathBuf = current_dir.join("src").join("user_data.txt");
+    let username_file_result = File::open(user_data_file);
 
     let mut username_file = match username_file_result {
         Ok(file) => file,
