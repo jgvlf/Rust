b42a39f2f0150ed395f83afab01afdc0b43819cc
jgvlf <joaogabrielvlf@gmail.com>
2025-01-06 14:31:33 -0300

    Rust - Error Handling - Use shortcut "?" to handle errors.

--- rust-error-handling/src/example/username.rs
+++ rust-error-handling/src/example/username.rs
@@ -5,17 +5,9 @@ use std::path::PathBuf;
 pub fn read_username_from_file() -> Result<String, io::Error> {
     let current_dir: PathBuf = std::env::current_dir().unwrap();
     let user_data_file: PathBuf = current_dir.join("src").join("user_data.txt");
-    let username_file_result = File::open(user_data_file);
-
-    let mut username_file = match username_file_result {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
+    let mut username_file_result = File::open(user_data_file)?;
 
     let mut username = String::new();
-
-    match username_file.read_to_string(&mut username) {
-        Ok(_) => Ok(username),
-        Err(e) => Err(e),
-    }
+    username_file_result.read_to_string(&mut username)?;
+    Ok(username)
 }
