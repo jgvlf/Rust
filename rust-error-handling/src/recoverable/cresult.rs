b99cf14fbbe1552afedf89ad74cd3872ebf0d821
jgvlf <joaogabrielvlf@gmail.com>
2025-01-06 10:33:54 -0300

    Rust - Error Handling - Handle File Creation and other possibles errors

--- rust-error-handling/src/recoverable/cresult.rs
+++ rust-error-handling/src/recoverable/cresult.rs
@@ -1,10 +1,19 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 pub fn open_file() {
     let greeting_file_result: Result<File, std::io::Error> = File::open("hello.txt");
 
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {error:?}"),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {e:?}"),
+            },
+            other_error => {
+                panic!("Problem opening the file: {other_error:?}")
+            }
+        },
     };
 }
