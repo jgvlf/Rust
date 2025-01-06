e3f2d22f34ee48c4c2273584ab1a4f6962321f6f
jgvlf <joaogabrielvlf@gmail.com>
2025-01-06 16:41:53 -0300

    Rust - Error Handling - Simplifier even more.

--- rust-error-handling/src/example/username.rs
+++ rust-error-handling/src/example/username.rs
@@ -3,11 +3,10 @@ use std::io::{self, Read};
 use std::path::PathBuf;
 
 pub fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username = String::new();
+
     let current_dir: PathBuf = std::env::current_dir().unwrap();
     let user_data_file: PathBuf = current_dir.join("src").join("user_data.txt");
-    let mut username_file_result = File::open(user_data_file)?;
-
-    let mut username = String::new();
-    username_file_result.read_to_string(&mut username)?;
+    File::open(user_data_file)?.read_to_string(&mut username)?;
     Ok(username)
 }
