b9ff6f2e46a9b8312cf46d5015e16900f2cff004
jgvlf <joaogabrielvlf@gmail.com>
2025-01-06 16:44:42 -0300

    Rust - Error Handling - use "fs" instead of the "File" struct

--- rust-error-handling/src/example/username.rs
+++ rust-error-handling/src/example/username.rs
@@ -1,12 +1,9 @@
-use std::fs::File;
-use std::io::{self, Read};
+use std::fs;
+use std::io;
 use std::path::PathBuf;
 
 pub fn read_username_from_file() -> Result<String, io::Error> {
-    let mut username = String::new();
-
     let current_dir: PathBuf = std::env::current_dir().unwrap();
     let user_data_file: PathBuf = current_dir.join("src").join("user_data.txt");
-    File::open(user_data_file)?.read_to_string(&mut username)?;
-    Ok(username)
+    fs::read_to_string(user_data_file)
 }
